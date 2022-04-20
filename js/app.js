af473aa2c5965f2e443f6290d30344425a136b6e
Lux <92374048+lucas38460@users.noreply.github.com>
2022-04-20 11:54:31 +0200

    add function for up and down

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let pe = document.getElementById('divCitation');
 const MoreQuote = document.getElementById('addQuote');
 const btnAdd = document.getElementById('addQuote');
+const BtnUp = document.getElementById('up');
+const BtnDown = document.getElementById('down');
 
 async function textrandomcitation () {
     fetch("https://api.kanye.rest/")
@@ -13,7 +15,20 @@ async function textrandomcitation () {
 };
 
 
-btn.addEventListener('click', () => {
-    document.createElement('li');
+btnAdd.addEventListener('click', () => {
+    let newDiv = document.createElement('div');
+    newDiv.className = "divNew";
     textrandomcitation();    
 });
+
+
+// // Btn Move Up
+// BtnUp.addEventListener('click', () => {
+
+// });
+
+
+// // Btn Move Down
+// BtnDown.addEventListener('click', () => {
+
+// });
