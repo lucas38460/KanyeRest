772738ad82ac2599db1db32851cc432e1a182175
Lux <92374048+lucas38460@users.noreply.github.com>
2022-04-21 15:24:47 +0200

    resolve problème creation de plusieurs li

--- js/app.js
+++ js/app.js
@@ -1,45 +1,47 @@
-let pe = document.getElementById('divCitation');
-const MoreQuote = document.getElementById('addQuote');
-const btnAdd = document.getElementById('addQuote');
-const BtnUp = document.getElementById('up');
-const BtnDown = document.getElementById('down');
+const MoreQuote = document.getElementById("addQuote");
+const btnAdd = document.getElementById("addQuote");
 let Double = [""];
 
+const ulMain = document.getElementById("UlMain");
 
-
-async function textrandomcitation () {
-    fetch("https://api.kanye.rest/")
-
-    .then(response => response.json())
+async function textrandomcitation() {
+  fetch("https://api.kanye.rest/")
+    .then((response) => response.json())
 
     .then((data) => {
-        pe.textContent = data.quote;
+      if (Double.includes(data.quote)) {
+        textrandomcitation();
+      } else {
+        let newDiv = document.createElement("li");
+        // newDiv.className = "divNew";
+        newDiv.textContent = data.quote;
+        const BtnUp = document.createElement("button");
+        BtnUp.textContent = "up";
+        const BtnDown = document.createElement("button");
+        BtnDown.textContent = "down";
+
+        newDiv.appendChild(BtnUp);
+        newDiv.appendChild(BtnDown);
+
+        UlMain.appendChild(newDiv);
+
+        Double.push(data.quote);
+      }
     });
-    // if (Double.includes(data.quote)) {
-    //     textrandomcitation();  
-    // } else {
-    //     let newDiv = document.createElement('div');
-    //     newDiv.className = "divNew";
-    // } 
-};
+}
 
-
-
-
-btnAdd.addEventListener('click', () => {
-    textrandomcitation();  
+btnAdd.addEventListener("click", () => {
+  textrandomcitation();
 });
-
 
 // // Btn Move Up
 // BtnUp.addEventListener('click', () => {
 
 // });
 
-
 // // Btn Move Down
 // BtnDown.addEventListener('click', () => {
 
 // });
 
-//.includes for test if this are in double in array 
+//.includes for test if this are in double in array
